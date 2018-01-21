84ff3c15b95ac2f11db8d4443f5c28efbe434e7f
sstaehler <simon.staehler@erdw.ethz.ch>
2018-01-21 15:58:33 +0100

    fix

--- seismogram_noise/__init__.py
+++ seismogram_noise/__init__.py
@@ -1,2 +1,2 @@
-from .seismogram_noise import create_noise, add_noise, \   # noqa
+from .seismogram_noise import create_noise, add_noise, \
                               get_spectrum, interpolation  # noqa
