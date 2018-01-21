86ac91f292a63feebac10483239fb9637527d204
sstaehler <simon.staehler@erdw.ethz.ch>
2018-01-21 13:15:31 +0100

    autoimport interpolation routine

--- seismogram_noise/__init__.py
+++ seismogram_noise/__init__.py
@@ -1,1 +1,2 @@
-from .seismogram_noise import create_noise, add_noise, get_spectrum  # noqa
+from .seismogram_noise import create_noise, add_noise, \   # noqa
+                              get_spectrum, interpolation  # noqa
