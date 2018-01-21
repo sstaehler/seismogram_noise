21dacafe211cb22cbdf57fe661bc131dade1e529
sstaehler <simon.staehler@erdw.ethz.ch>
2018-01-21 13:13:28 +0100

    PEP8

--- seismogram_noise/tests/test.py
+++ seismogram_noise/tests/test.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -------------------------------------------------------------------
+#   Filename:  test.py
+#   Purpose:   Tests for seismogram_noise
+#   Author:    Simon Staehler
+#   Email:     mail@simonstaehler.com
+#   License:   MIT License
+# -------------------------------------------------------------------
+
 import obspy
 import seismogram_noise
 import matplotlib.pyplot as plt
@@ -12,11 +23,13 @@ for model in ['STS2', 'Tcompact', 'CMG40T-OBS', 'NHNM', 'NLNM']:
     # Add noise
     seismogram_noise.add_noise(st, model=model)
     power, f = mlab.psd(x=st[0].data, Fs=st[0].stats.sampling_rate, NFFT=2**14)
-    plt.plot(1./f[f>0], 10*np.log10(power[f>0]), label='resulting spectrum')
+    plt.plot(1./f[f > 0], 10*np.log10(power[f > 0]),
+             label='resulting spectrum')
 
     # Add theoretical spectrum
     spec = seismogram_noise.get_spectrum(model)
-    plt.plot(1./spec[0], 10*np.log10(spec[1]), 'r', label='theory', lw=2)
+    plt.plot(1./spec[0], 10*np.log10(spec[1]), 'r',
+             label='theory', lw=2)
 
     plt.xlim(0.1, 500)
     plt.xscale('log')
